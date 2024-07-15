46ad51703bcda24a92497f681c30da9ff802cc5f
Naresh Bhosale <nareshbhosale73@gmail.com>
2024-07-15 21:21:48 +0530

    animation added

--- src/components/custom/ToggleButton.tsx
+++ src/components/custom/ToggleButton.tsx
@@ -1,29 +1,56 @@
 import { Moon, Sun } from "lucide-react";
+import React from "react";
 import { useTheme } from "../../context/ThemeProvider";
 import { Button } from "../ui/button";
 
 const ToggleButton = () => {
   const { setTheme, theme } = useTheme();
+  console.log(`🚀 ~ file: ToggleButton.tsx:8 ~ theme:`, theme);
+  const ref = React.useRef<HTMLDivElement>(null);
 
   return (
-    <Button
-      onClick={() => {
-        if (theme === "light") {
-          setTheme("dark");
-        } else {
-          setTheme("light");
-        }
-      }}
-      variant="ghost"
-      size="icon"
-    >
-      {theme === "light" ? (
-        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
-      ) : (
-        <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
-      )}
-      <span className="sr-only">Toggle theme</span>
-    </Button>
+    <div className="relative">
+      <Button
+        onClick={() => {
+          if (theme === "dark") {
+            ref.current?.classList.add(
+              "!scale-[150]",
+              "!bg-white",
+              "!opacity-[0.7]"
+            );
+          } else {
+            ref.current?.classList.add(
+              "!scale-[150]",
+              "!bg-black",
+              "!opacity-[0.7]"
+            );
+          }
+          setTimeout(() => {
+            setTheme(theme === "light" ? "dark" : "light");
+            ref.current?.classList.remove(
+              "!scale-[150]",
+              "!bg-white",
+              "!bg-black",
+              "!opacity-[0.7]"
+            );
+          }, 1000);
+        }}
+        variant="ghost"
+        size="icon"
+      >
+        <div
+          ref={ref}
+          className={`absolute h-[30px] w-[30px] rounded-full z-20 transition-all duration-1000`}
+        ></div>
+
+        {theme === "light" ? (
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+        ) : (
+          <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+        )}
+        <span className="sr-only">Toggle theme</span>
+      </Button>
+    </div>
   );
 };
 
